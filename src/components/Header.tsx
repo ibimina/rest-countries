190a4d7f3cb08b7db658260f4b0dae72fba19abc
Ibimina <ibimina.c.hart@gmail.com>
2023-02-01 00:07:01 +0100

    docs: added image attributes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,8 @@ function Header() {
     return (<header>
         <h1>Where in the world?</h1>
         <div className="theme">
-            <Image src="" alt="" />
+            <Image src="/theme.svg" alt="moon" width={50} height={50}
+                />
             <p className="theme-text">Dark Mode</p>
         </div>
     </header>);
