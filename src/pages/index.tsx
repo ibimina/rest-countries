933d0043f8014bd0c3b63ad9025aec50452b02d3
Ibimina <ibimina.c.hart@gmail.com>
2023-02-02 01:55:07 +0100

    docs: refactored code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -19,23 +19,31 @@ type CountryProps = {
 export default function Home({ countries }: { countries: CountryProps }) {
   const handleSearch = (e: React.FormEvent<HTMLInputElement>) => {
     setInputValue(e.currentTarget.value)
-    setCountriesData((data) => data.filter((country) => country.name.toLowerCase().includes(inputValue.toLowerCase())))
-  }
-  const [countriesData, setCountriesData] = useState(countries)
-  const [inputValue, setInputValue] = useState("")
-  
-  const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
-
-    setCountriesData((data) => data.filter((country) => country.region === e.target.value))
-    if (e.target.value === '') return setCountriesData(countries)
-
-  }
-  useEffect(()=>{
-    if (inputValue.length === 0) {
-      setCountriesData(countries)
+    Array.from(document.querySelectorAll('.country-name')).forEach((p) => {
+      if (p.textContent?.toLowerCase().includes(inputValue.toLowerCase())) {
+        p.parentElement?.parentElement?.classList.remove('hidden')
+      } else {
+        p.parentElement?.parentElement?.classList.add('hidden')
+      }
     }
-   
-  },[inputValue,countries])
+    )
+
+
+  }
+
+  const [inputValue, setInputValue] = useState("")
+
+  const handleFilter = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    Array.from(document.querySelectorAll('.region')).forEach((p) => {
+      if (p.textContent === e.target.value) {
+        p.parentElement?.parentElement?.classList.remove('hidden')
+      } else {
+        p.parentElement?.parentElement?.classList.add('hidden')
+      }
+    }
+    )
+  }
+
   return (
     <>
       <Head>
@@ -45,29 +53,35 @@ export default function Home({ countries }: { countries: CountryProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <main className={styles.container}>
+        <div className='flex-input'>
+          <input type="text" value={inputValue} onChange={handleSearch} placeholder="search for a country" className={styles.input} />
+          <select name="filtercountry" id="" onChange={handleFilter} className={styles.select}>
+            <option value="">Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="America">America</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
+        <ul className={styles.lists}>
+          {countries.map((country) => (
+            <Link key={country.name} href={`/${encodeURIComponent(country.name)}`} className={styles.link}>
+              <div className='imgcon'>
+                <Image src={country.flags.svg} alt="flag" fill sizes='(max-height:450px)' className='flag'/>
+              </div>
+              <div className={styles.countrydet}>
+                <p className={`${styles.countryname} country-name`}>{country.name}</p>
+                <p><span className='bold'>Polpulation</span>: <span className='light'>{country.population}</span> </p>
+                <p className='region'> <span className='bold'>Region</span> :<span className='light'>{country.region}</span></p>
+                <p> <span className='bold'>Capital</span>: <span className='light'>{country.capital}</span></p>
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </main>
 
-      <div className='flex-input'>
-        <input type="text" value={inputValue} onChange={handleSearch} placeholder="search for a country" />
-        <select name="filtercountry" id="" onChange={handleFilter}>
-          <option value="">Filter by Region</option>
-          <option value="Africa">Africa</option>
-          <option value="America">America</option>
-          <option value="Asia">Asia</option>
-          <option value="Europe">Europe</option>
-          <option value="Oceania">Oceania</option>
-        </select>
-      </div>
-      <ul>
-        {countriesData.map((country) => (
-          <Link key={country.name} href={`/${encodeURIComponent(country.name)}`}>
-            <Image src={country.flags.svg} alt="flag" width={100} height={100} />
-            <p>{country.name}</p>
-            <p>{country.population}</p>
-            <p>{country.region}</p>
-            <p>{country.capital}</p>
-          </Link>
-        ))}
-      </ul>
     </>
   )
 }
