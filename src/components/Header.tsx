a5ab6ac44bd61a2006ce3cc5bf63afa7c4bfa799
Ibimina <ibimina.c.hart@gmail.com>
2023-02-02 01:53:07 +0100

    docs: added image width and height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
     return (<header>
         <h1>Where in the world?</h1>
         <div className="theme">
-            <Image src="/theme.svg" alt="moon" width={50} height={50}
+            <Image src="/theme.svg" alt="moon" width={20} height={20}
                 />
             <p className="theme-text">Dark Mode</p>
         </div>
