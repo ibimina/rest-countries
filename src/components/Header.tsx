4cc2f89fa496afb9534d76d71b70a18c202cfb18
ibimina <96787210+ibimina@users.noreply.github.com>
2023-02-02 14:58:28 +0100

    docs: toggle theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
 import Image from "next/image";
-
+import { useState } from "react";
 function Header() {
+    const [mode, setMode] = useState("Light Mode")
+    const changeTheme = (e:React.MouseEvent<HTMLImageElement>) =>{
+      
+        if (mode ==="Light Mode") {
+            document.documentElement.classList.add("dark")
+            document.documentElement.classList.remove("light")
+            setMode("Dark Mode")
+    }else{
+            document.documentElement.classList.add("light") 
+            document.documentElement.classList.remove("dark")
+            setMode("Light Mode")
+    }
+    }
     return (<header>
         <h1>Where in the world?</h1>
         <div className="theme">
             <Image src="/theme.svg" alt="moon" width={20} height={20}
-                />
-            <p className="theme-text">Dark Mode</p>
+               onClick={changeTheme} />
+            <p className="theme-text">{mode}</p>
         </div>
     </header>);
 }
